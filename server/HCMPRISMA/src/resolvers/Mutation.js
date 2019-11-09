ab72c151f098665ed9f650c8b156c7d545b22376
hcmuleva <hcmuleva@gmail.com>
2019-11-09 16:54:37 +0530

    update user including password is fixed

--- server/HCMPRISMA/src/resolvers/Mutation.js
+++ server/HCMPRISMA/src/resolvers/Mutation.js
@@ -1,14 +1,10 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
-
+import hashPassword from '../utils/hashPassword'
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
-        if (args.data.password.length < 8) {
-            throw new Error('Password must be 8 characters or longer.')
-        }
-
-        const password = await bcrypt.hash(args.data.password, 10)
+        const password = await hashPassword(args.data.password)
         const user = await prisma.mutation.createUser({
             data: {
                 ...args.data,
@@ -54,7 +50,9 @@ const Mutation = {
     },
     async updateUser(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
-
+        if(typeof args.data.password === 'string'){
+            args.data.password=await hashPassword(args.data.password)
+        }
         return prisma.mutation.updateUser({
             where: {
                 id: userId
@@ -86,11 +84,9 @@ const Mutation = {
                 id: userId
             }
         })
-
         if (!postExists) {
             throw new Error('Unable to delete post')
-        }
-        
+        }    
         return prisma.mutation.deletePost({
             where: {
                 id: args.id
