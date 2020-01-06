df28cde070b63953049936643e16bec3364f1d4c
Harish Muleva <hcmuleva@gmail.com>
2020-01-06 11:20:09 +0530

    removed keys due to security

--- client/src/components/fileuploads/keys.js
+++
@@ -1,7 +0,0 @@
-export const aws ={
-    accessKeyId:"AKIAJJCW4M4QS27SAYMQ",
-    secretAccessKey:"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",
-    region:"ap-southeast-1",
-    dirName:"education",
-    bucketName:"zbimages"
-}
